92b6c64158df75ad307e847256b7f797a3a73a28
maycol <lopezmaycol743@gmail.com>
2024-03-13 22:52:45 -0500

    se agregra el codigo del ejercicio

---
+++ prueba.js
@@ -0,0 +1,19 @@
+var numero1 =0;
+var numero2 =0;
+var resultado =0;
+
+numero1 = parseInt(prompt("Ingrese el valor del numero1"));
+numero2 = parseInt(prompt("Ingrese el valor del numero2"));
+
+if(control>=numero1){
+  resultado = numero1 + 1;
+}
+
+if(control>=numero2){
+    resultado = numero2 + 1;
+}
+
+alert("numero "+numero1+" y el numero"+numero1+"son amigos")
+
+
+
